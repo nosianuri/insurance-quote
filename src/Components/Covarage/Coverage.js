84022d607675ea1bd5c4551c49d6dd0f3ce503f9
nosianuri <nurinosia@gmail.com>
2023-01-28 23:22:13 +0600

    update coverage img

--- src/Components/Covarage/Coverage.js
+++ src/Components/Covarage/Coverage.js
@@ -12,7 +12,7 @@ const Coverage = () => {
             <p className='mb-3'>When it comes to your insurance policies, premium rates aren’t always your first concern. You need the right coverage options to protect your assets thoroughly. While other insurance agencies offer savings and discounts that seem promising, they typically break down your coverage to the bare minimum. That’s where Insurance Trendy Quote Insurance makes a difference. </p>
             <p>Insurance Trendy Quote brings clients affordable rates with policies that don’t sacrifice your coverage and protection. When you meet with our brokers, you’ll see exactly what you get with your policy. We’ll help you compare your options, sort through various premium rates, and pick the right coverage for your unique needs. With Insurance Trendy Quote, we’re dedicated to ensuring every one of our clients walks away with confidence and peace of mind with the right coverage at the best prices. </p>
           </div>
-          <img src="https://i.ibb.co/hx09LKB/services-show-image.webp" alt="" />
+          <img src="https://i.ibb.co/q5RxTFd/about.png" alt="" />
         </div>
       </div>
     </div>
