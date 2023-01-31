0e17e4fa7a7ad083e3716ebb64c88b1e6be065e9
nosianuri <nurinosia@gmail.com>
2023-02-01 00:20:28 +0600

    fixed banner

--- src/Components/Banner/BannerBody.js
+++ src/Components/Banner/BannerBody.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const BannerBody = () => {
   return (
-    <div className='bg-[#3b3352] lg:pt-36 '>
-        <div className='grid lg:grid-cols-2 grid-cols-1 lg:px-32 px-3 lg:gap-32 gap-10'>
+    <div className='bg-[#3b3352] lg:pt-36 md:pt-28'>
+        <div className='grid lg:grid-cols-2 grid-cols-1 lg:pl-20 md:pl-5 px-3 lg:gap-32 gap-10'>
             <div className='lg:w-[650px] order-last md:order-first lg:order-first'>
                 <h1 className='uppercase lg:text-5xl md:text-3xl text-2xl font-semibold !leading-normal text-white lg:text-left text-center'>Protect Yourself, Your Family And The Life You’ve Built</h1>
                 <p className='text-white mt-3 lg:text-left text-center'>Our comprehensive coverage options ensure you get the coverage you need to protect your most valuable assets. Talk to our expert brokers to get exclusive policy rates that meet your needs.</p>
