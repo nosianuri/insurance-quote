6f250d3b514bc1523b1662037c82ce1b09a3aabd
nosianuri <nurinosia@gmail.com>
2023-02-02 04:18:08 +0600

    Update blog text

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -8,68 +8,75 @@ import category5 from '../../assets/catagories5.png'
 import category6 from '../../assets/deals1.png'
 
 const Category = () => {
-  return (
-    <section class="categories" id="shop">
-        <div class="container">
-            {/* <h1 class="categories-title">Stories & tips
+    return (
+        <section class="categories" id="shop">
+            <div class="container">
+                {/* <h1 class="categories-title">Stories & tips
 for everyday life
 previous</h1> */}
 
-            <div class="categories-container mt-16">
-                <div class="categories-item">
-                    <div class="overly-conteiner">
-                        <img src={category1} alt="A foggy Golden Gate Bridge" />
-                        <div class="overly">
-                            <h3>Wemens Pants</h3>
-                            <button>Explore <i class="fa fa-long-arrow-right"></i></button>
-                        </div>
-                    </div>
-                </div>
-                <div class="categories-item categories-item2">
-                    <div class="overly-conteiner">
-                        <img src={category2} alt="A foggy Golden Gate Bridge" />
-                        <div class="overly">
-                            <h3>Mans Jeckets</h3>
-                            <button>Explore <i class="fa fa-long-arrow-right"></i></button>
-                        </div>
-                    </div>
-                    <div class="overly-conteiner">
-                        <img src={category3} alt="A foggy Golden Gate Bridge" />
-                        <div class="overly">
-                            <h3>Wemens Tops</h3>
-                            <button>Explore <i class="fa fa-long-arrow-right"></i></button>
-                        </div>
-                    </div>
-                </div>
-                <div class="categories-item categories-item2">
-                    <div class="overly-conteiner">
-                        <img src={category4} alt="A foggy Golden Gate Bridge" />
-                        <div class="overly">
-                            <h3>Sutars</h3>
-                            <button>Explore <i class="fa fa-long-arrow-right"></i></button>
-                        </div>
-                    </div>
-                    <div class="categories-item3">
+                <div class="categories-container mt-16">
+                    <div class="categories-item">
                         <div class="overly-conteiner">
-                            <img src={category5} alt="A foggy Golden Gate Bridge" />
+                            <img src={category1} alt="A foggy Golden Gate Bridge" />
                             <div class="overly">
-                                <h3>Pants</h3>
+                                <h3 className='font-semibold'>
+                                    Top 7 Insurance Coverages To Protect Your Business.
+                                </h3>
+                                <button>Explore <i class="fa fa-long-arrow-right"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="categories-item categories-item2">
+                        <div class="overly-conteiner">
+                            <img src={category2} alt="A foggy Golden Gate Bridge" />
+                            <div class="overly">
+                                <h3 className='font-semibold'>How Are Driverless Cars Going To Impact The Insurance Industry?
+                                    </h3>
                                 <button>Explore <i class="fa fa-long-arrow-right"></i></button>
                             </div>
                         </div>
                         <div class="overly-conteiner">
-                            <img src={category6} alt="A foggy Golden Gate Bridge" />
+                            <img src={category3} alt="A foggy Golden Gate Bridge" />
                             <div class="overly">
-                                <h3>Jeckets</h3>
+                                <h3 className='font-semibold'>
+                                    Why Renter’s Insurance Should Be Reviewed Annually</h3>
                                 <button>Explore <i class="fa fa-long-arrow-right"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="categories-item categories-item2">
+                        <div class="overly-conteiner">
+                            <img src={category4} alt="A foggy Golden Gate Bridge" />
+                            <div class="overly">
+                                <h3 className='font-semibold'>
+                                    What Are The Major Prerequisites For Buying A Home In BC?
+                                </h3>
+                                <button>Explore <i class="fa fa-long-arrow-right"></i></button>
+                            </div>
+                        </div>
+                        <div class="categories-item3">
+                            <div class="overly-conteiner">
+                                <img src={category5} alt="A foggy Golden Gate Bridge" />
+                                <div class="overly">
+                                    <h3 className='font-semibold'>Insurance Acquires Ken Rystead
+                                        </h3>
+                                    <button>Explore <i class="fa fa-long-arrow-right"></i></button>
+                                </div>
+                            </div>
+                            <div class="overly-conteiner">
+                                <img src={category6} alt="A foggy Golden Gate Bridge" />
+                                <div class="overly">
+                                    <h3 className='font-semibold'>The Perils Of Distracted Driving</h3>
+                                    <button>Explore <i class="fa fa-long-arrow-right"></i></button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Category
