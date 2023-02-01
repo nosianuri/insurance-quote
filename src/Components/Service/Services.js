cc91ed0a312e133ee46a4136f4b90d0580b210b1
nosianuri <nurinosia@gmail.com>
2023-02-02 03:41:42 +0600

    fix site image

--- src/Components/Service/Services.js
+++ src/Components/Service/Services.js
@@ -6,7 +6,7 @@ const Services = (props) => {
         <div className='flex '>
             <div className='flex flex-col bg-white rounded-lg shadow-md hover:shadow-2xl w-full m-6 overflow-hidden border-4 border-white border-b-[#2d2640]'>
             <a href={link}>
-            <img src={img} alt="" className='h-20 w-16 my-6 mx-auto' />
+            <img src={img} alt="" className='h-16 w-16 my-6 mx-auto' />
             </a>
                 <a href={link}>
                 <h2 className='head-title text-center px-2 font-bold text-xl mb-2'>{name}</h2>
