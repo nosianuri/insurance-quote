3cdac6a7cf4b2b44e6d3f0314612f53bfdbd8802
nosianuri <nurinosia@gmail.com>
2023-02-02 15:09:30 +0600

    set get coverage

--- src/Components/Compare/CompareCard.js
+++ src/Components/Compare/CompareCard.js
@@ -2,35 +2,38 @@ import React from 'react'
 
 const CompareCard = () => {
     return (
-        <div className='my-10 mx-32'>
-            <div className='lg:flex max-w-7xl mx-auto justify-center items-center'>
-                <div className='p-16 border-2 border-l-[#e3e3e3] '>
+        <div className='my-10 lg:mx-32 mx-16'>
+            <div className='grid lg:grid-cols-2 grid-cols-1 max-w-7xl mx-auto justify-center items-center border-2 border-l-[#e3e3e3]'>
+                <div className='text-center'>
                     <h2 className='text-3xl inline-block align-middle'>Insure today, be sure tomorrow.</h2>
                 </div>
-                <div className='p-5 h-[165px] border-2 border-l-[#e3e3e3] align-middle'>
-                    <div className='mt-5'>
-                    <div className='text-center mb-5'>
-                        <i class="fa-solid fa-file-invoice text-[#2d2640] text-5xl "></i>
+                <div className='lg:flex md:flex '>
+                    <div className='p-5 border border-white border-l-[#e3e3e3] align-middle'>
+                        <div className='mt-5'>
+                            <div className='text-center mb-5'>
+                                <i class="fa-solid fa-file-invoice text-[#2d2640] text-5xl "></i>
+                            </div>
+                            <h4 className='text-center head-title uppercase text-lg font-bold text-[#2d2640]'>Top Carriers</h4>
+                        </div>
                     </div>
-                    <h4 className='text-center head-title uppercase text-lg font-bold text-[#2d2640]'>Top Carriers</h4>
+                    <div className='p-5 border border-white border-l-[#e3e3e3]'>
+                        <div className='mt-5'>
+                            <div className='text-center mb-5'>
+                                <i class="fa-solid fa-arrow-up-right-dots text-[#2d2640] text-5xl "></i>
+                            </div>
+                            <h4 className='text-center head-title uppercase text-lg font-bold text-[#2d2640]'>Compare Quotes</h4>
+                        </div>
+                    </div>
+                    <div className='p-5  border border-white border-l-[#e3e3e3]'>
+                        <div className='mt-5'>
+                            <div className='text-center mb-5'>
+                                <i class="fa-solid fa-sack-dollar text-[#2d2640] text-5xl "></i>
+                            </div>
+                            <h4 className='text-center head-title uppercase text-lg font-bold text-[#2d2640]'>Save Time & Money</h4>
+                        </div>
                     </div>
                 </div>
-                <div className='p-5 h-[165px] border-2 border-l-[#e3e3e3] '>
-                   <div className='mt-5'>
-                   <div className='text-center mb-5'>
-                        <i class="fa-solid fa-arrow-up-right-dots text-[#2d2640] text-5xl "></i>
-                    </div>
-                    <h4 className='text-center head-title uppercase text-lg font-bold text-[#2d2640]'>Compare Quotes</h4>
-                   </div>
-                </div>
-                <div className='p-5 h-[165px] border border-l-[#e3e3e3]'>
-                    <div className='mt-5'>
-                    <div className='text-center mb-5'>
-                        <i class="fa-solid fa-sack-dollar text-[#2d2640] text-5xl "></i>
-                    </div>
-                    <h4 className='text-center head-title uppercase text-lg font-bold text-[#2d2640]'>Save Time & Money</h4>
-                    </div>
-                </div>
+
             </div>
 
         </div>
--- src/Components/Cta/Cta.js
+++ src/Components/Cta/Cta.js
@@ -6,9 +6,9 @@ const Cta = () => {
   return (
     <div className='bg-[#201e44] lg:mx-32 mx-3'>
       <div className='grid lg:grid-cols-2 grid-cols-1 justify-center items-center'>
-        <div className='text-center px-10 '>
-          <h2 className='head-title font-semibold lg:text-2xl text-2xl !leading-tight mb-3 text-white'>Get The Coverage You Need And The Protection You Deserve!</h2>
-          <p className='my-3 text-white'>Ready to get started? Our agents are waiting to help you explore your insurance coverage options.</p>
+        <div className='text-center px-10 my-5 order-2'>
+          <h2 className='head-title font-semibold lg:text-2xl text-xl !leading-tight mb-3 text-white'>Get The Coverage You Need And The Protection You Deserve!</h2>
+          <p className='my-3 text-white lg:text-md text-xs'>Ready to get started? Our agents are waiting to help you explore your insurance coverage options.</p>
           <a href="/#get-quote">
             <button
               padding="10px 15px"
@@ -24,7 +24,7 @@ const Cta = () => {
           {/* <p className='lg:text-2xl font-bold mt-3 text-white hover:text-[#2d2640]'><a href="tel:18445010362">(844) 501-0362
           </a></p> */}
         </div>
-        <div>
+        <div className='lg:order-2'>
           <img className='lg:order-last' src={cta} alt="" />
         </div>
       </div>
