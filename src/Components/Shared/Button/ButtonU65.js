5f5b9863b42dbcc8d2eb9d66d3e3131e77f6668f
nosianuri <nurinosia@gmail.com>
2023-05-18 03:20:11 +0600

    update u65 number

--- src/Components/Shared/Button/ButtonU65.js
+++ src/Components/Shared/Button/ButtonU65.js
@@ -4,7 +4,7 @@ const ButtonU65 = () => {
   return (
     <div className='text-center'>
       <p className='text-sm lg:block hidden'>Our Agents can help you save</p>
-      <a href="tel:+18445942106"><p className='text-[#635ce1] font-semibold lg:text-lg text-xs flex justify-center items-center'><i className="fa-solid fa-phone-volume pr-3"></i><span className='lg:block '>(844) 5942106</span></p></a>
+      <a href="tel:+18445942106"><p className='text-[#635ce1] font-semibold lg:text-lg text-xs flex justify-center items-center'><i className="fa-solid fa-phone-volume pr-3"></i><span className='lg:block '>(844) 5942-106</span></p></a>
     </div>
   )
 }
